455c0d8b4acdc5fa212418a87572bf42bfbb3b1a
wakadorimk2 <wakadori.mk2@gmail.com>
2024-03-31 05:50:58 +0900

    型エイリアスを追加しました

--- app.ts
+++ app.ts
@@ -1,9 +1,10 @@
 type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text';
 
 function combine(
     input1: Combinable,
     input2: Combinable,
-    resultConversion: 'as-number' | 'as-text',
+    resultConversion: ConversionDescriptor,
 ) {
     let result;
     if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
