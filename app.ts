5197c3f5be06052e63ad4d86a6f51b783b2b4959
wakadorimk2 <wakadori.mk2@gmail.com>
2024-03-30 20:08:50 +0900

    変数personの型を修正しました

--- app.ts
+++ app.ts
@@ -1,7 +1,13 @@
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string]; // Tuple
+} = {
     name: 'John',
     age: 30,
     hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
 };
 
 console.log(person);
