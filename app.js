b261ae969ec4156613dbc563b66e108d403184a1
wakadorimk2 <wakadori.mk2@gmail.com>
2024-03-31 03:33:31 +0900

    Add Role enum and check if person is an admin

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: 'John',
     age: 30,
     hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN
 };
 console.log(person);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
+}
+if (person.role === Role.ADMIN) {
+    console.log('管理者ユーザ');
 }
--- app.ts
+++ app.ts
@@ -1,12 +1,22 @@
+enum Role {
+    ADMIN,
+    READ_ONLY,
+    AUTHOR
+}
+
 const person = {
     name: 'John',
     age: 30,
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
+    role: Role.ADMIN
 };
 
 console.log(person);
 
 for (const hobby of person.hobbies) {
     console.log(hobby.toUpperCase());
+}
+
+if (person.role === Role.ADMIN) {
+    console.log('管理者ユーザ');
 }
