cc8e2b1f8056a49617762431bcb1169843ac1f59
wakadorimk2 <wakadori.mk2@gmail.com>
2024-04-03 16:11:35 +0900

    変更点: app.jsにコードの変更があります。add関数を削除し、新たにuserInputとuserName変数を追加しました。また、generateError関数を追加し、エラーメッセージとエラーコードをスローするようにしました。

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Max';
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-function printResult(num) {
-    console.log('Result: ' + num);
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
 }
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-});
-var combineValues;
-combineValues = add;
-console.log(combineValues(8, 8));
-printResult(add(5, 12));
+generateError('エラーが発生しました', 500);
