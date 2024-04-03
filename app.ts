fbcf59fde1a843f35d368db69c198bdf33e72719
wakadorimk2 <wakadori.mk2@gmail.com>
2024-04-03 15:55:16 +0900

    関数addAndHandleを追加しました

--- app.ts
+++ app.ts
@@ -6,6 +6,11 @@ function printResult(num: number): void {
     console.log('Result: ' + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+    const result = n1 + n2;
+    cb(result);
+}
+
 let combineValues: (a: number, b: number) => number;
 
 combineValues = add;
