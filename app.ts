72430a611b223a65fa4994c098f6435738918e65
wakadorimk2 <wakadori.mk2@gmail.com>
2024-04-03 15:21:15 +0900

    関数の結果を出力する前に、新しい関数を追加しました。

--- app.ts
+++ app.ts
@@ -6,4 +6,9 @@ function printResult(num: number): void {
     console.log('Result: ' + num);
 }
 
+let combineValues;
+combineValues = add;
+
+console.log(combineValues(8, 8));
+
 printResult(add(5, 12));
