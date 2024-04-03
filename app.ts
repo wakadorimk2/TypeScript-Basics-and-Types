060b43907d9013b051cf06f2597e424a13cbb036
wakadorimk2 <wakadori.mk2@gmail.com>
2024-04-03 15:25:32 +0900

    関数の型を修正しました

--- app.ts
+++ app.ts
@@ -6,9 +6,10 @@ function printResult(num: number): void {
     console.log('Result: ' + num);
 }
 
-let combineValues: Function;
+let combineValues: (a: number, b: number) => number;
 
 combineValues = add;
+combineValues = printResult;
 combineValues = 5;
 
 console.log(combineValues(8, 8));
