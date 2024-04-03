44ecca6260dc566e9e50d92ec953e9f1db525f98
wakadorimk2 <wakadori.mk2@gmail.com>
2024-04-03 14:52:05 +0900

    関数printResultの型アノテーションを追加

--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@ function add(n1: number, n2: number) {
     return n1 + n2;
 }
 
-function printResult(num: number) {
+function printResult(num: number): void {
     console.log('Result: ' + num);
 }
 
